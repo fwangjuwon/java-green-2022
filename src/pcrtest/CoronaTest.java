849161ba1ff838349c9166cce7e515e0d3d17ed5
fwangjuwon <fwangjuwon@naver.com>
2022-02-18 15:58:43 +0900

    20220218 pcr검사 가능 데이터 db전송완료

--- src/pcrtest/CoronaTest.java
+++ src/pcrtest/CoronaTest.java
@@ -15,9 +15,10 @@ public class CoronaTest {
 
     public static void main(String[] args) {
 
+        String addr = "http://apis.data.go.kr/B551182/rprtHospService/getRprtHospService?serviceKey=wJmmW29e3AEUjwLioQR22CpmqS645ep4S8TSlqtSbEsxvnkZFoNe7YG1weEWQHYZ229eNLidnI2Yt5EZ3Stv7g%3D%3D&pageNo=1&numOfRows=5190&_type=json";
+
         try {
-            URL url = new URL(
-                    "http://apis.data.go.kr/B551182/rprtHospService/getRprtHospService?serviceKey=wJmmW29e3AEUjwLioQR22CpmqS645ep4S8TSlqtSbEsxvnkZFoNe7YG1weEWQHYZ229eNLidnI2Yt5EZ3Stv7g%3D%3D&pageNo=1&numOfRows=5190&_type=json");
+            URL url = new URL(addr);
 
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
@@ -34,22 +35,22 @@ public class CoronaTest {
             Connection conndb = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "SCOTT", "TIGER");
             System.out.println("db연결 했어!");
 
-            String sql = "INSERT INTO userTbl(addr, mgtStaDd, pcrPsblYn, ratPsblYn, recuClCd, rprtWorpClicFndtTgtYn, sgguCdNm, sidoCdNm, telno, XPos, XPosWgs84, YPos, YPosWgs84, yadmNm, ykihoEnc) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+            String sql = "INSERT INTO Hospital(addr, mgtStaDd, pcrPsblYn, ratPsblYn, recuClCd, rprtWorpClicFndtTgtYn, sgguCdNm, sidoCdNm, telno, XPos, XPosWgs84, YPos, YPosWgs84, yadmNm, ykihoEnc) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
             PreparedStatement pstmt = conndb.prepareStatement(sql);
 
-            for (int i = 0; i < result.size(); i++) {
+            for (int i = result.size() - 1; i >= 0; i--) {
                 pstmt.setString(1, result.get(i).getAddr());
-                pstmt.setLong(2, result.get(i).getMgtStaDd());
+                pstmt.setString(2, result.get(i).getMgtStaDd());
                 pstmt.setString(3, result.get(i).getPcrPsblYn());
                 pstmt.setString(4, result.get(i).getRatPsblYn());
-                pstmt.setLong(5, result.get(i).getRecuClCd());
+                pstmt.setString(5, result.get(i).getRecuClCd());
                 pstmt.setString(6, result.get(i).getRprtWorpClicFndtTgtYn());
                 pstmt.setString(7, result.get(i).getSgguCdNm());
                 pstmt.setString(8, result.get(i).getSidoCdNm());
                 pstmt.setString(9, result.get(i).getTelno());
-                pstmt.setLong(10, result.get(i).getXPos());
+                pstmt.setString(10, result.get(i).getXPos());
                 pstmt.setString(11, result.get(i).getXPosWgs84());
-                pstmt.setLong(12, result.get(i).getYPos());
+                pstmt.setString(12, result.get(i).getYPos());
                 pstmt.setString(13, result.get(i).getYPosWgs84());
                 pstmt.setString(14, result.get(i).getYadmNm());
                 pstmt.setString(15, result.get(i).getYkihoEnc());
@@ -65,7 +66,7 @@ public class CoronaTest {
             }
 
         } catch (Exception e) {
-            System.out.println("조회오류" + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
--- src/pcrtest/Hospital.java
+++ src/pcrtest/Hospital.java
@@ -1,26 +1,164 @@
 package pcrtest;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
 public class Hospital {
 
-    private String addr; // 주소
-    private long mgtStaDd; // 운영시작일자
+    private String addr; // 의료기관 주소
+    private String mgtStaDd; // 운영시작일자
     private String pcrPsblYn; // 구분코드
     private String ratPsblYn; // RAT가능여부
-    private long recuClCd;// 요양종별코드
-    private String rprtWorpClicFndtTgtYn;// 호흡기전담클리닉여부
-    private String sgguCdNm;// 시군구명
-    private String sidoCdNm;// 시도명
-    private String telno;// 전화번호
-    private long XPos;// x좌표
-    private String XPosWgs84;// 세계지구x좌표
-    private long YPos;// y좌표
-    private String YPosWgs84;// 세계지구y좌표
-    private String yadmNm;// 요양기관명
-    private String ykihoEnc;// 암호화된요양기호
+    private String recuClCd; // 요양종별코드
+    private String rprtWorpClicFndtTgtYn; // 호흡기전담클리닉 여부
+    private String sgguCdNm; // 시군구명
+    private String sidoCdNm; // 시도명
+    private String telno; // 전화번호
+    private String XPos; // x좌표
+    private String XPosWgs84; // 세계지구 x좌표
+    private String YPos;// y좌표
+    private String YPosWgs84; // 세계지구 y좌표
+    private String yadmNm; // 요양기관명
+    private String ykihoEnc; // 암호화된 요양기호
+
+    public Hospital(String addr, String mgtStaDd,
+            String pcrPsblYn, String ratPsblYn, String recuClCd, String rprtWorpClicFndtTgtYn, String sgguCdNm,
+            String sidoCdNm, String telno, String XPos, String XPosWgs84, String Ypos, String YPosWgs84, String yadmNm,
+            String ykihoEnc) {
+
+        this.addr = addr;
+        this.mgtStaDd = mgtStaDd;
+        this.pcrPsblYn = pcrPsblYn;
+        this.ratPsblYn = ratPsblYn;
+        this.recuClCd = recuClCd;
+        this.rprtWorpClicFndtTgtYn = rprtWorpClicFndtTgtYn;
+        this.sgguCdNm = sgguCdNm;
+        this.sidoCdNm = sidoCdNm;
+        this.telno = telno;
+        this.XPos = XPos;
+        this.XPosWgs84 = XPosWgs84;
+        this.YPos = Ypos;
+        this.YPosWgs84 = YPosWgs84;
+        this.yadmNm = yadmNm;
+        this.ykihoEnc = ykihoEnc;
+
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    public String getMgtStaDd() {
+        return mgtStaDd;
+    }
+
+    public void setMgtStaDd(String mgtStaDd) {
+        this.mgtStaDd = mgtStaDd;
+    }
+
+    public String getPcrPsblYn() {
+        return pcrPsblYn;
+    }
+
+    public void setPcrPsblYn(String pcrPsblYn) {
+        this.pcrPsblYn = pcrPsblYn;
+    }
+
+    public String getRatPsblYn() {
+        return ratPsblYn;
+    }
+
+    public void setRatPsblYn(String ratPsblYn) {
+        this.ratPsblYn = ratPsblYn;
+    }
+
+    public String getRecuClCd() {
+        return recuClCd;
+    }
+
+    public void setRecuClCd(String recuClCd) {
+        this.recuClCd = recuClCd;
+    }
+
+    public String getRprtWorpClicFndtTgtYn() {
+        return rprtWorpClicFndtTgtYn;
+    }
+
+    public void setRprtWorpClicFndtTgtYn(String rprtWorpClicFndtTgtYn) {
+        this.rprtWorpClicFndtTgtYn = rprtWorpClicFndtTgtYn;
+    }
+
+    public String getSgguCdNm() {
+        return sgguCdNm;
+    }
+
+    public void setSgguCdNm(String sgguCdNm) {
+        this.sgguCdNm = sgguCdNm;
+    }
+
+    public String getSidoCdNm() {
+        return sidoCdNm;
+    }
+
+    public void setSidoCdNm(String sidoCdNm) {
+        this.sidoCdNm = sidoCdNm;
+    }
+
+    public String getTelno() {
+        return telno;
+    }
+
+    public void setTelno(String telno) {
+        this.telno = telno;
+    }
+
+    public String getXPos() {
+        return XPos;
+    }
+
+    public void setXPos(String xPos) {
+        XPos = xPos;
+    }
+
+    public String getXPosWgs84() {
+        return XPosWgs84;
+    }
+
+    public void setXPosWgs84(String xPosWgs84) {
+        XPosWgs84 = xPosWgs84;
+    }
+
+    public String getYPos() {
+        return YPos;
+    }
+
+    public void setYPos(String yPos) {
+        YPos = yPos;
+    }
+
+    public String getYPosWgs84() {
+        return YPosWgs84;
+    }
+
+    public void setYPosWgs84(String yPosWgs84) {
+        YPosWgs84 = yPosWgs84;
+    }
+
+    public String getYadmNm() {
+        return yadmNm;
+    }
+
+    public void setYadmNm(String yadmNm) {
+        this.yadmNm = yadmNm;
+    }
+
+    public String getYkihoEnc() {
+        return ykihoEnc;
+    }
+
+    public void setYkihoEnc(String ykihoEnc) {
+        this.ykihoEnc = ykihoEnc;
+    }
 
 }
